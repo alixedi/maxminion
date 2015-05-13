19280df12ba5698cfafb2c33451f7babc91d655c
Ali Zaidi <azaidi@maxeler.com>
2015-05-13 16:25:26 +0100

    Created folder.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 maxminion
 =========
 
-A test minion that compiles the given apps using MaxCompiler 2015.1 release and produces a report.
+A stupid, yellow assistant for testing MaxCompiler.
 
-    $ python minion.py <username> <password>
+It doesn't talk nonsense though - for now.
 
-Where username and password are for the source control e.g. GitHub.
+Usage
+-----
 
-This is the first draft, alpha version that is broken by design. Here are some of the problems:
+    $ git clone https://github.com/alixedi/maxminion
 
-* Takes the username and password for the source control in plain text on the CLI.
-* Creates a tmp directory for cloning the apps. In case of an error, no clean-up is performed resulting in possible inconsistencies in following runs.
-* Redirects the output of the compilation process to STDNULL and produces a minimal report saying 'OK' or 'ERROR'.
-* Have not tested this with SVN yet. Should work though. For obvious reasons.
+    $ pip install docopt
+   
+    $ python minion.py
+
---
+++ maxminion/minion.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+    maxminion.minion
+    ~~~~~~~~~~~~~~~~
+
+    A stupid, yellow assistant for testing MaxCompiler.
+
+    It doesn't talk nonsense though.
+
+    :copyright: (c) 2015 Maxeler Technologies
+    :license: No license at the moment.
+"""
+
+_doc_ = """
+Maxeler Test Minion.
+
+Usage:
+  maxminion <instructions_file>
+  maxminion -h | --help
+  maxminion --version
+
+Options:
+  -h --help    Show this screen.
+  --version    Show version.
+"""
+
+from os import path
+from docopt import docopt
+from utils import mkcd, load_inst, get_login
+from utils import fetch_code, build_code, uprint
+from utils import CodeFetchError, CodeBuildError
+
+
+VERSION = '0.0.1'
+FILE = path.realpath(__file__)
+DIR = path.dirname(FILE)
+SCRATCH = path.join(DIR, '.scratch')
+
+
+def run_app_test(app_name, app, login):
+    """Runs tests for given app."""
+    uprint('Testing %s... ' % app_name)
+    with mkcd(app_name):
+        try:
+            fetch_code(app['FETCH'], login)
+            build_code(app['BUILD'])
+            print('OK')
+        except CodeFetchError as e:
+            print(e)
+        except CodeBuildError as e:
+            print(e)
+
+def run_tests(apps, login):
+    """Kinda main. Runs the whole thing."""
+    with mkcd(SCRATCH):
+        # TODO: multiprocessing?
+        for app_name in apps:
+            app = apps[app_name]
+            run_app_test(app_name, app, login)
+
+if __name__ == '__main__':
+    args = docopt(_doc_, version=VERSION)
+    ins = load_inst(args['<instructions_file>'])
+    run_tests(ins['APPS'], get_login())
+
